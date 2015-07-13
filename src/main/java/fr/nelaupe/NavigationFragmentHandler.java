407081bc49117c9ecfc3a049ca58ed3f2380e753
Lucas Nelaupe <lucas34990@gmail.com>
2015-07-13 21:51:56 +0800

    Add copyright

--- src/main/java/fr/nelaupe/NavigationFragmentHandler.java
+++ src/main/java/fr/nelaupe/NavigationFragmentHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015-present Lucas Nelaupe
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package fr.nelaupe;
 
 import android.app.Activity;
@@ -8,7 +24,7 @@ import android.os.Bundle;
 
 /**
  * Created with IntelliJ
- * Created by lucas
+ * Created by Lucas Nelaupe
  * Date 24/03/15
  */
 @SuppressWarnings("unused")
